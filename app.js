8048b0cd8d22a69d9a1bc0d5d1b20f22fbe0af12
NICOLA-200 <fatepepe66@gmail.com>
2023-08-28 21:49:43 +0200

    multer working well

--- app.js
+++ app.js
@@ -3,22 +3,7 @@ const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 
-// Set The Storage Engine
-const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
 
-// Init Upload
-const upload = multer({
-  storage: storage,
-  // limits:{fileSize: 1000000},
-  // fileFilter: function(req, file, cb){
-  //   checkFileType(file, cb);
-  // }
-}).single('myImage');
 
 
 
