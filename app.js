37c84bf36d15a888e5f5d6b8c5778e7f613ec2db
NICOLA-200 <fatepepe66@gmail.com>
2023-08-28 21:48:42 +0200

    added the first config

--- app.js
+++ app.js
@@ -47,27 +47,4 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.render('index'));
 
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
-        msg: err
-      });
-    } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        res.render('index', {
-          msg: 'File Uploaded!',
-          file: `uploads/${req.file.filename}`
-        });
-      }
-    }
-  });
-});
 
-const port = 3000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
