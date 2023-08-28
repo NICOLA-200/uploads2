58a52edbf5f075e71aa1e57bc950b0995dfe9c0c
NICOLA-200 <fatepepe66@gmail.com>
2023-08-28 21:49:26 +0200

    removed the bug

--- app.js
+++ app.js
@@ -20,26 +20,7 @@ const upload = multer({
   // }
 }).single('myImage');
 
-// Check File Type
-// function checkFileType(file, cb){
-//   // Allowed ext
-//   const filetypes = /jpeg|jpg|png|gif/;
-//   // Check ext
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//   // Check mime
-//   const mimetype = filetypes.test(file.mimetype);
 
-//   if(mimetype && extname){
-//     return cb(null,true);
-//   } else {
-//     cb('Error: Images Only!');
-//   }
-// }
-
-// Init app
-const app = express();
-
-// EJS
 
 
 
